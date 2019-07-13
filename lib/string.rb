903baa64fb1bc497bd7ebafd109aaa2853fce57b
小楼一夜听春雨 <guixu0912@163.com>
2019-07-14 00:42:57 +0800

    Add files via upload

---
+++ lib/string.rb
@@ -0,0 +1,21 @@
+# 找出字符串中连续出现次数大于1的字符串
+arr = "aaasdfasdasddffddffaassddfasdfaacccccddd".split("")
+len = 1
+a = []
+arr.each_with_index do |x,i|
+	if i > 0
+		if x == arr[i-1]
+			len += 1
+			if x != arr[i+1]
+				a << arr[i-1] * len
+				len = 1
+			end
+		else
+			if len > 1
+				a << arr[i-1] * len
+				len = 1
+			end
+		end
+	end
+end
+p a
