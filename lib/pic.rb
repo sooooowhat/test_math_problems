0b05eac98c712094af6a0888ac97386065d8374d
小楼一夜听春雨 <guixu0912@163.com>
2019-10-29 17:12:07 +0800

    Update pic.rb

--- lib/pic.rb
+++ lib/pic.rb
@@ -1,9 +1,17 @@
 require 'open-uri'
-def downLoadJpg(fileName, uri)
-  data = open(uri, 'User-Agent' => 'ruby'){|f| f.read}
+require 'rails'
+def downLoadJpg(fileName, url)
+  data = open(url, 'User-Agent' => 'ruby'){|f| f.read}
   file = File.new fileName, 'w+'
   file.binmode
   file << data
   file.flush
   file.close
+end
+
+arr = ['pic_link'] * n
+
+arr.each do |pic|
+  next if pic.blank?
+  downLoadJpg(pic.split('/')[-1], pic)
 end
